5e7aec3c695209aa3561cf0089596d1b3e775534
Bastien Clément <g@ledric.me>
2017-09-14 20:58:01 +0200

    No need to implement local blob cache since HTTP cache can be used instead

--- server/app/controllers/AddonsController.scala
+++ server/app/controllers/AddonsController.scala
@@ -23,17 +23,41 @@ class AddonsController @Inject()(userAction: UserAction, checkAcl: CheckAcl, ws:
 		Ok(views.html.addons.list())
 	}
 
-	private def proxyRequest(key: String, url: String, mime: String): Action[AnyContent] = addonsAction.async {
-		wsCache.getOrElseUpdate(key, 15.minutes) {
-			ws.url(url).get().collect {
-				case res if res.status == 200 => Ok(res.bodyAsBytes).as(mime)
-			}
-		}.recover { case _ => NotFound }
+	private def proxyRequest(key: String, url: String, mime: String,
+	                         immutable: Boolean = false): Action[AnyContent] = {
+		addonsAction.async {
+			wsCache.getOrElseUpdate(key, 15.minutes) {
+				ws.url(url).get().collect {
+					case res if res.status == 200 =>
+						Ok(res.bodyAsBytes).as(mime).withHeaders(
+							"Cache-Control" -> {
+								if (immutable) "public, max-age=3600, immutable"
+								else "no-cache, no-store, must-revalidate"
+							}
+						)
+				}
+			}.recover { case _ => NotFound }
+		}
 	}
 
-	def manifest = proxyRequest("addons:manifest", "http://addons.wfit.ovh/manifest.php", "application/json")
-	def digest(addon: String) = proxyRequest(s"addons:digest:$addon", s"http://addons.wfit.ovh/repository/$addon.digest.json", "application/json")
-	def blob(hash: String) = proxyRequest(s"addons:blob:$hash", s"http://addons.wfit.ovh/blobs/$hash", "application/octet-stream")
+	def manifest = proxyRequest(
+		"addons:manifest",
+		"http://addons.wfit.ovh/manifest.php",
+		"application/json"
+	)
+
+	def digest(addon: String) = proxyRequest(
+		s"addons:digest:$addon",
+		s"http://addons.wfit.ovh/repository/$addon.digest.json",
+		"application/json"
+	)
+
+	def blob(hash: String) = proxyRequest(
+		s"addons:blob:$hash",
+		s"http://addons.wfit.ovh/blobs/$hash",
+		"application/octet-stream",
+		immutable = true
+	)
 
 	def notifyUpdate = Action.async {
 		wsCache.removeAll().map(_ => Ok) andThen { case _ => eventBus.publish("updater.notify", ()) }
