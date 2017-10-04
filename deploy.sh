c7a1cc5fd31a828fc506368debbfa3af8b30ebba
Bastien Clément <g@ledric.me>
2017-10-04 03:38:57 +0200

    Always clean before deploying

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sbt docker:stage | pv -t -N compile -i 0.1
+sbt clean docker:stage | pv -t -N compile -i 0.1
 cd server/target/docker
 tar -zcvf stage.tar.gz stage | pv -pt -l -s $(find stage | wc -l) -i 0.1 -N compress > /dev/null
 pv -N upload -i 0.1 -pter stage.tar.gz | ssh debian@app.wfit.ovh "
